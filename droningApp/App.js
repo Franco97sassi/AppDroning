bc774a58133ec7677c4c72cff64308ad514213e0
franco sassi <franco.sassi97@gmail.com>
2024-08-14 16:57:40 -0300

    splashScreen

--- droningApp/App.js
+++ droningApp/App.js
@@ -1,8 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function App() {
+  useEffect(() => {
+    const hideSplashScreen=async ()=>{
+      await SplashScreen.hideAsync()
+      
+    }
+    hideSplashScreen();
+  }, [])
+  
   return (
     // <View style={styles.container}>
        <AppNavigator/>
